936ace97a5a87a4d29f4bde30cda2e1a05d7529d
vamp-jjang <jayhannvillarin@gmail.com>
2025-05-10 13:01:45 +0800

    Pasting the Maps Platform API Key to android and ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyDZuK3iJMP6qCiPf-MzsGVK7e5XEDPWW58")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
